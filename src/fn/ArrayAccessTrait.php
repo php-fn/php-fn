82c344d883dc7b52910b212f9b7aba15807ac146
Demjan Kaluzki <kaluzkidemjan@gmail.com>
2019-05-09 00:31:32 +0200

    extract ArrayAccess implementation to the ArrayAccessTrait

---
+++ src/fn/ArrayAccessTrait.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Copyright (C) php-fn. See LICENSE file for license details.
+ */
+
+namespace fn;
+
+/**
+ */
+trait ArrayAccessTrait
+{
+    /**
+     * @var array
+     */
+    private $data;
+
+    /**
+     * @return array
+     */
+    private function data(): array
+    {
+        return is_array($this->data) ? $this->data : $this->data = [];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function offsetExists($offset): bool
+    {
+        return hasKey($offset, $this->data());
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function offsetGet($offset)
+    {
+        hasKey($offset, $data = $this->data()) || fail\argument($offset);
+        return $data[$offset];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function offsetSet($offset, $value): void
+    {
+        $this->data();
+        $this->data[$offset] = $value;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function offsetUnset($offset): void
+    {
+        $this->data();
+        unset($this->data[$offset]);
+    }
+}
--- src/fn/Map.php
+++ src/fn/Map.php
@@ -6,22 +6,25 @@
 namespace fn;
 
 use ArrayAccess;
+use Closure;
 use Countable;
 use IteratorAggregate;
 
 /**
- * @property-read array $keys
- * @property-read array $traverse
- * @property-read array $values
- * @property-read array $tree
- * @property-read array $leaves
- * @property-read array $flatten
+ * @property-read array  $keys
+ * @property-read array  $traverse
+ * @property-read array  $values
+ * @property-read array  $tree
+ * @property-read array  $leaves
+ * @property-read array  $flatten
  * @property-read string $string
- * @property-read bool $every
- * @property-read bool $some
+ * @property-read bool   $every
+ * @property-read bool   $some
  */
 class Map implements IteratorAggregate, Countable, ArrayAccess
 {
+    use ArrayAccessTrait;
+
     /**
      * @var iterable
      */
@@ -31,11 +34,6 @@ class Map implements IteratorAggregate, Countable, ArrayAccess
      * @var callable
      */
     private $mappers;
-
-    /**
-     * @var array
-     */
-    private $compiled;
 
     /**
      * @var Map\Tree
@@ -58,13 +56,13 @@ class Map implements IteratorAggregate, Countable, ArrayAccess
      */
     public function __get($property)
     {
-        switch($property) {
+        switch ($property) {
             case 'keys':
                 return traverse($this->keys());
             case 'traverse':
-                return is_array($this->compiled) ? $this->compiled : traverse($this);
+                return is_array($this->data) ? $this->data : traverse($this);
             case 'values':
-                return _\toValues(is_array($this->compiled) ? $this->compiled : $this);
+                return _\toValues(is_array($this->data) ? $this->data : $this);
             case 'tree':
                 return _\toValues($this->tree());
             case 'leaves':
@@ -74,57 +72,17 @@ class Map implements IteratorAggregate, Countable, ArrayAccess
             case 'string':
                 return $this->string();
             case 'every':
-                return $this->every(function($value) {return $value;});
+                return $this->every(static function ($value) {
+                    return $value;
+                });
             case 'some':
-                return $this->some(function($value) {return $value;});
+                return $this->some(static function ($value) {
+                    return $value;
+                });
             default:
                 fail\logic($property);
         }
         return null;
-    }
-
-    /**
-     * @param string|callable|array $delimiter
-     *
-     * @param array $replacements
-     *
-     * @return string
-     */
-    public function string($delimiter = PHP_EOL, ...$replacements): string
-    {
-        $string = '';
-        if (!isCallable($delimiter)) {
-            if (is_array($delimiter)) {
-                array_unshift($replacements, $delimiter);
-                $delimiter = PHP_EOL;
-            }
-            $delimiter = function($counter) use($delimiter) {
-                return $counter ? $delimiter : '';
-            };
-        }
-        traverse($this->leaves(function($value, Map\Path $iterator) use ($delimiter, &$string) {
-            static $counter = 0;
-            $string .= $delimiter(...[$counter++, $iterator->getDepth(), $iterator]) . $value;
-
-        }));
-        return $replacements ? str($string, ...$replacements) : $string;
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function __toString(): string
-    {
-        return $this->string();
-    }
-
-    /**
-     * @param string $property
-     * @return bool
-     */
-    public function __isset($property): bool
-    {
-        return hasValue($property, ['keys', 'traverse', 'map', 'values', 'tree', 'leaves', 'string', 'every', 'some']);
     }
 
     /**
@@ -136,141 +94,12 @@ class Map implements IteratorAggregate, Countable, ArrayAccess
     }
 
     /**
-     * @inheritdoc
-     */
-    public function __unset($property): void
-    {
-        fail\logic($property);
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function getIterator(): \Traversable
-    {
-        if (!$this->inner) {
-            if (is_array($this->compiled)) {
-                $this->inner = new Map\Tree($this->compiled);
-            } else {
-                $this->inner = new Map\Tree($this->iterable ?: [], ...$this->mappers);
-            }
-        }
-        return $this->inner;
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function count(): int
-    {
-        return $this->getIterator()->count();
-    }
-
-    /**
-     * @return bool
-     */
-    public function isLast(): bool
-    {
-        return $this->getIterator()->isLast();
-    }
-
-    /**
-     * @return array
-     */
-    private function compile(): array
-    {
-        if (!is_array($this->compiled)) {
-            $this->compiled = traverse($this);
-            $this->inner = null;
-            $this->mappers = [];
-        }
-        return $this->compiled;
-    }
-
-    /**
-     * @param mixed $value
-     * @param bool $strict
-     * @return bool
-     */
-    public function has($value, $strict = true): bool
-    {
-        return hasValue($value, $this->compile(), $strict);
-    }
-
-    /**
-     * @param string $key
-     * @param mixed $default
-     * @return mixed|null
-     */
-    public function get($key, $default = null)
-    {
-        return $this->offsetExists($key) ? $this->offsetGet($key) : $default;
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function offsetExists($offset): bool
-    {
-        return hasKey($offset, $this->compile());
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function offsetGet($offset)
-    {
-        $this->offsetExists($offset) ?: fail\argument($offset);
-        return $this->compiled[$offset];
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function offsetSet($offset, $value): void
-    {
-        $this->compile();
-        $this->compiled[$offset] = $value;
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function offsetUnset($offset): void
-    {
-        $this->compile();
-        unset($this->compiled[$offset]);
-    }
-
-    /**
-     * @param callable ...$mappers
-     * @return static
-     */
-    public function then(callable ...$mappers): self
-    {
-        return new static(
-            is_array($this->compiled) ? $this->compiled : $this->iterable,
-            ...$this->mappers, ...$mappers
-        );
-    }
-
-    /**
-     * @param callable|int $strategy callable or SORT_ constants
-     * @param int $flags SORT_ constants
-     * @return static
-     */
-    public function sort($strategy = null, $flags = null): self
-    {
-        return new static(new Map\Sort($this, $strategy, $flags));
-    }
-
-    /**
      * @param callable ...$mappers
      * @return static
      */
     public function keys(callable ...$mappers): self
     {
-        return $this->then(function ($value, $key)  {
+        return $this->then(static function ($value, $key) {
             static $counter = 0;
             return mapValue($key)->andKey($counter++);
         }, ...$mappers);
@@ -280,75 +109,12 @@ class Map implements IteratorAggregate, Countable, ArrayAccess
      * @param callable ...$mappers
      * @return static
      */
-    public function values(callable ...$mappers): self
+    public function then(callable ...$mappers): self
     {
-        return $this->then(function ($value)  {
-            static $counter = 0;
-            return mapValue($value)->andKey($counter++);
-        }, ...$mappers);
-    }
-
-    /**
-     * @param callable $function
-     * @param array ...$iterables
-     * @return \Closure
-     */
-    private static function variadic($function, ...$iterables): \Closure
-    {
-        return function () use ($function, $iterables) {
-            return $function(...traverse($iterables, function ($iterable) {
-                return _\toArray($iterable);
-            }));
-        };
-    }
-
-    /**
-     * @param iterable ...$iterables
-     * @return static
-     */
-    public function merge(...$iterables): self
-    {
-        return new static(new Map\Lazy(self::variadic('array_merge', $this, ...$iterables)));
-    }
-
-    /**
-     * @param iterable ...$iterables
-     * @return static
-     */
-    public function replace(...$iterables): self
-    {
-        return new static(new Map\Lazy(self::variadic('array_replace', $this, ...$iterables)));
-    }
-
-    /**
-     * @param iterable ...$iterables
-     * @return static
-     */
-    public function diff(...$iterables): self
-    {
-        return new static(new Map\Lazy(self::variadic('array_diff', $this, ...$iterables)));
-    }
-
-    /**
-     * @param int $start
-     * @param int $length
-     * @return static
-     */
-    public function sub($start, $length = null): self
-    {
-        return new static(new Map\Lazy(function () use($start, $length) {
-            return sub($this, $start, $length);
-        }));
-    }
-
-    /**
-     * @param mixed $needle
-     * @param bool $strict
-     * @return false|int|string
-     */
-    public function search($needle, $strict = true)
-    {
-        return array_search($needle, $this->compile(), $strict);
+        return new static(
+            is_array($this->data) ? $this->data : $this->iterable,
+            ...$this->mappers, ...$mappers
+        );
     }
 
     /**
@@ -373,13 +139,13 @@ class Map implements IteratorAggregate, Countable, ArrayAccess
 
     /**
      * @param callable $mapper
-     * @param string $glue
+     * @param string   $glue
      *
      * @return static
      */
     public function flatten(callable $mapper = null, string $glue = '/'): self
     {
-        return $this->tree(function(Map\Path $it, $value) use($glue, $mapper) {
+        return $this->tree(static function (Map\Path $it, $value) use ($glue, $mapper) {
             $key = implode($glue, $it->keys);
             $mapper && $value = $mapper(...[$value, &$key, $it]);
             if ($value instanceof Map\Value) {
@@ -387,6 +153,33 @@ class Map implements IteratorAggregate, Countable, ArrayAccess
             }
             return $value === null ? $value : mapKey($key)->andValue($value);
         });
+    }
+
+    /**
+     * @param string|callable|array $delimiter
+     *
+     * @param array                 $replacements
+     *
+     * @return string
+     */
+    public function string($delimiter = PHP_EOL, ...$replacements): string
+    {
+        $string = '';
+        if (!isCallable($delimiter)) {
+            if (is_array($delimiter)) {
+                array_unshift($replacements, $delimiter);
+                $delimiter = PHP_EOL;
+            }
+            $delimiter = static function ($counter) use ($delimiter) {
+                return $counter ? $delimiter : '';
+            };
+        }
+        traverse($this->leaves(static function ($value, Map\Path $iterator) use ($delimiter, &$string) {
+            static $counter = 0;
+            $string .= $delimiter(...[$counter++, $iterator->getDepth(), $iterator]) . $value;
+
+        }));
+        return $replacements ? str($string, ...$replacements) : $string;
     }
 
     /**
@@ -424,6 +217,180 @@ class Map implements IteratorAggregate, Countable, ArrayAccess
     }
 
     /**
+     * @inheritdoc
+     */
+    public function __toString(): string
+    {
+        return $this->string();
+    }
+
+    /**
+     * @param string $property
+     * @return bool
+     */
+    public function __isset($property): bool
+    {
+        return hasValue($property, ['keys', 'traverse', 'map', 'values', 'tree', 'leaves', 'string', 'every', 'some']);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function __unset($property): void
+    {
+        fail\logic($property);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function count(): int
+    {
+        return $this->getIterator()->count();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getIterator(): Map\Tree
+    {
+        if (!$this->inner) {
+            if (is_array($this->data)) {
+                $this->inner = new Map\Tree($this->data);
+            } else {
+                $this->inner = new Map\Tree($this->iterable ?: [], ...$this->mappers);
+            }
+        }
+        return $this->inner;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isLast(): bool
+    {
+        return $this->getIterator()->isLast();
+    }
+
+    /**
+     * @param mixed $value
+     * @param bool  $strict
+     * @return bool
+     */
+    public function has($value, $strict = true): bool
+    {
+        return hasValue($value, $this->data(), $strict);
+    }
+
+    /**
+     * @return array
+     */
+    private function data(): array
+    {
+        if (!is_array($this->data)) {
+            $this->data    = traverse($this);
+            $this->inner   = null;
+            $this->mappers = [];
+        }
+        return $this->data;
+    }
+
+    /**
+     * @param string $key
+     * @param mixed  $default
+     * @return mixed|null
+     */
+    public function get($key, $default = null)
+    {
+        return $this->offsetExists($key) ? $this->offsetGet($key) : $default;
+    }
+
+    /**
+     * @param callable|int $strategy callable or SORT_ constants
+     * @param int          $flags    SORT_ constants
+     * @return static
+     */
+    public function sort($strategy = null, $flags = null): self
+    {
+        return new static(new Map\Sort($this, $strategy, $flags));
+    }
+
+    /**
+     * @param callable ...$mappers
+     * @return static
+     */
+    public function values(callable ...$mappers): self
+    {
+        return $this->then(static function ($value) {
+            static $counter = 0;
+            return mapValue($value)->andKey($counter++);
+        }, ...$mappers);
+    }
+
+    /**
+     * @param iterable ...$iterables
+     * @return static
+     */
+    public function merge(...$iterables): self
+    {
+        return new static(new Map\Lazy(self::variadic('array_merge', $this, ...$iterables)));
+    }
+
+    /**
+     * @param callable $function
+     * @param array    ...$iterables
+     * @return Closure
+     */
+    private static function variadic($function, ...$iterables): Closure
+    {
+        return static function () use ($function, $iterables) {
+            return $function(...traverse($iterables, static function ($iterable) {
+                return _\toArray($iterable);
+            }));
+        };
+    }
+
+    /**
+     * @param iterable ...$iterables
+     * @return static
+     */
+    public function replace(...$iterables): self
+    {
+        return new static(new Map\Lazy(self::variadic('array_replace', $this, ...$iterables)));
+    }
+
+    /**
+     * @param iterable ...$iterables
+     * @return static
+     */
+    public function diff(...$iterables): self
+    {
+        return new static(new Map\Lazy(self::variadic('array_diff', $this, ...$iterables)));
+    }
+
+    /**
+     * @param int $start
+     * @param int $length
+     * @return static
+     */
+    public function sub($start, $length = null): self
+    {
+        return new static(new Map\Lazy(function () use ($start, $length) {
+            return sub($this, $start, $length);
+        }));
+    }
+
+    /**
+     * @param mixed $needle
+     * @param bool  $strict
+     * @return false|int|string
+     */
+    public function search($needle, $strict = true)
+    {
+        return array_search($needle, $this->data(), $strict);
+    }
+
+    /**
      * @param int $limit
      * @param int $offset
      *
@@ -433,7 +400,7 @@ class Map implements IteratorAggregate, Countable, ArrayAccess
     {
         $from = max($offset, 0);
         $to   = $from + max($limit, 0);
-        return $this->then(function($value) use($from, $to) {
+        return $this->then(static function ($value) use ($from, $to) {
             static $count = 0;
             if ($count < $from) {
                 $value = null;
