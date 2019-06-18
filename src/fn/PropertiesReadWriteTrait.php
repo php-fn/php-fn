fd94047df600cc68ae68366359b0e628aa6cffb4
Demjan Kaluzki <kaluzkidemjan@gmail.com>
2019-06-18 11:48:46 +0200

    add dynamic property resolve strategy to the PropertiesReadWriteTrait

--- src/fn/PropertiesReadWriteTrait.php
+++ src/fn/PropertiesReadWriteTrait.php
@@ -3,8 +3,12 @@
  * Copyright (C) php-fn. See LICENSE file for license details.
  */
 /** @noinspection PhpUndefinedClassConstantInspection */
+/** @noinspection PhpDocMissingThrowsInspection */
+/** @noinspection PhpUnhandledExceptionInspection */
 
 namespace fn;
+
+use ReflectionMethod;
 
 /**
  * @see http://php.net/manual/language.oop5.overloading.php#language.oop5.overloading.members
@@ -34,23 +38,64 @@ trait PropertiesReadWriteTrait
 
     /**
      * @param string $name
+     *
+     * @return ReflectionMethod|false
+     */
+    private function propertyMethod(string $name)
+    {
+        static $methods = [];
+        if (!isset($methods[$name])) {
+            $method = [static::class, "resolve$name"];
+            $methods[$name] =  method_exists(...$method) ? new ReflectionMethod(...$method) : false;
+        }
+        return $methods[$name];
+    }
+
+    /**
+     * @param string $name
+     * @param mixed ...$args
+     *
+     * @return mixed
+     */
+    private function propertyMethodInvoke(string $name, ...$args)
+    {
+        return $this->{$this->propertyMethod($name)->name}(...$args);
+    }
+
+    /**
+     * @param string $name
      * @param bool   $assert
-     * @param mixed  ...$value
+     * @param mixed  ...$args
      *
      * @return $this|mixed
      */
-    private function property(string $name, bool $assert, ...$value)
+    private function property(string $name, bool $assert, ...$args)
     {
         $has = hasKey($name, $this->properties);
+
         if ($assert) {
-            $has || fail\domain('missing magic-property %s in %s', $name, static::class);
-            if ($value) {
-                $this->properties[$name] = $value[0];
+            if (!($has || $this->propertyMethod($name))) {
+                fail\domain('missing magic-property %s in %s', $name, static::class);
+            }
+            if ($args) {
+                if ($method = $this->propertyMethod($name)) {
+
+                    $method->getNumberOfParameters() > 0 || fail\domain(
+                        'class %s has read-only access for the magic-property: %s',
+                        static::class,
+                        $name
+                    );
+
+                    $this->propertyMethodInvoke($name, ...$args);
+                } else {
+                    $this->properties[$name] = $args[0];
+                }
                 return $this;
             }
-            return $this->properties[$name];
+            return $has ? $this->properties[$name] : $this->propertyMethodInvoke($name);
         }
-        return $has;
+
+        return $has || $this->propertyMethod($name);
     }
 
     /**
--- tests/fn/PropertiesReadWriteTraitTest.php
+++ tests/fn/PropertiesReadWriteTraitTest.php
@@ -11,6 +11,7 @@ class PropertiesReadWrite
 {
     use PropertiesReadWriteTrait;
     private const DEFAULT = ['a' => null];
+
     public function __construct($properties)
     {
         $this->initProperties($properties);
@@ -29,35 +30,100 @@ class PropertiesReadWriteTraitTest extends \PHPUnit\Framework\TestCase
     public function testTrait(): void
     {
         assert\exception(
-            str('magic properties (b,c) are not defined in %s::DEFAULT',PropertiesReadWrite::class),
-            function () {new PropertiesReadWrite(['b' => 'B', 'a' => 'A', 'c' => 'C']);}
+            str('magic properties (b,c) are not defined in %s::DEFAULT', PropertiesReadWrite::class),
+            static function () {
+                new PropertiesReadWrite(['b' => 'B', 'a' => 'A', 'c' => 'C']);
+            }
         );
 
-        self::assertAB('A', null, new class(['a' => 'A', 'b' => null]) {
+        self::assertAB('A', null, new class(['a' => 'A', 'b' => null])
+        {
             use PropertiesReadWriteTrait;
+
             public function __construct($properties)
             {
                 $this->initProperties($properties);
             }
         });
 
-        self::assertAB('A', null, new class {
+        self::assertAB('A', null, new class
+        {
             use PropertiesReadWriteTrait;
             protected const DEFAULT = ['a' => 'A', 'b' => null];
+
             public function __construct()
             {
                 $this->initProperties();
             }
         });
 
-        self::assertAB('A', 'B', new class(['b' => 'B']) {
+        self::assertAB('A', 'B', new class(['b' => 'B'])
+        {
             use PropertiesReadWriteTrait;
+
             public function __construct($properties)
             {
                 $this->initProperties($properties);
             }
+
             private const DEFAULT = ['a' => 'A', 'b' => null];
         });
+
+        self::assertAB('A', 'B', $obj = new class
+        {
+            use PropertiesReadWriteTrait;
+
+            protected function resolveA(...$args): string
+            {
+                $args && $this->properties['a'] = $args[0];
+                return $this->properties['a'] ?? 'A';
+            }
+
+            protected function resolveB(...$args): string
+            {
+                $args && $this->properties['b'] = $args[0];
+                return $this->properties['b'] ?? 'B';
+            }
+
+            protected function resolveD(): string
+            {
+                return 'D';
+            }
+        });
+        assert\same('D', $obj->d);
+        assert\exception(
+            str('class %s has read-only access for the magic-property: d', get_class($obj)),
+            static function ($obj) {
+               $obj->d = 'd';
+            },
+            $obj
+        );
+
+
+        $obj = new class {
+            use PropertiesReadWriteTrait;
+
+            private $unresolved = [];
+            protected function propertyMethodInvoke(string $name, ...$args)
+            {
+                if ($args) {
+                    $this->unresolved[$name] = $args;
+                    return $args[0];
+                }
+                return $this->{$this->propertyMethod($name)->name}(...$this->unresolved[$name] ?? []);
+            }
+
+            protected function resolveProp(...$args): array
+            {
+                return [$args[0] ?? 'A'];
+            }
+        };
+
+        assert\same(['A'], $obj->prop);
+        unset($obj->unknown, $obj->prop);
+        assert\same(['A'], $obj->prop);
+        $obj->prop = 'B';
+        assert\same(['B'], $obj->prop);
     }
 
     private static function assertAB($a, $b, $obj): void
@@ -67,7 +133,7 @@ class PropertiesReadWriteTraitTest extends \PHPUnit\Framework\TestCase
         assert\false(isset($obj->c));
         assert\same($a, $obj->a);
         assert\same($b, $obj->b);
-        assert\exception(str('missing magic-property c in %s', get_class($obj)), function ($obj) {
+        assert\exception(str('missing magic-property c in %s', get_class($obj)), static function ($obj) {
             $obj->c;
         }, $obj);
         unset($obj->a, $obj->b, $obj->c);
