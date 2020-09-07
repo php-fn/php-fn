9811ac3b8176fd78291b08d4e510e46d75d3ebf1
Demjan Kaluzki <kaluzkidemjan@gmail.com>
2020-09-07 12:41:46 +0200

    Gen::map handles array-typed keys correctly

--- src/Php/Gen.php
+++ src/Php/Gen.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 /**
  * Copyright (C) php-fn. See LICENSE file for license details.
  */
@@ -36,6 +36,9 @@ class Gen implements IteratorAggregate
                 if ($map) {
                     $values = $map($value, $key, $pos);
                     $values instanceof Generator || $values = [$key => $values];
+                } else if (is_array($key)) {
+                    self::group($key, $value, $data);
+                    continue;
                 } else {
                     $values = [$key => $value];
                 }
@@ -44,23 +47,7 @@ class Gen implements IteratorAggregate
                         break 3;
                     }
                     if (is_array($k)) {
-                        if (count($k) === 1) {
-                            yield $k[0] => $v;
-                            continue;
-                        }
-                        $data === null && $data = [];
-                        $count = count($k);
-                        $groups = &$data;
-                        foreach ($k as $i => $group) {
-                            if ($i + 1 < $count) {
-                                isset($groups[$group]) || $groups[$group] = [];
-                                $groups = &$groups[$group];
-                            } else if ($group === null) {
-                                $groups[] = $v;
-                            } else {
-                                $groups[$group] = $v;
-                            }
-                        }
+                        self::group($k, $v, $data);
                     } else if ($k === null) {
                         yield $numKey++ => $v;
                     } else {
@@ -72,6 +59,23 @@ class Gen implements IteratorAggregate
         }
         if (is_array($data)) {
             yield from $data;
+        }
+    }
+
+    private static function group($k, $v, &$data): void
+    {
+        $data === null && $data = [];
+        $count = count($k);
+        $groups = &$data;
+        foreach ($k as $i => $group) {
+            if ($i + 1 < $count) {
+                isset($groups[$group]) || $groups[$group] = [];
+                $groups = &$groups[$group];
+            } else if ($group === null) {
+                $groups[] = $v;
+            } else {
+                $groups[$group] = $v;
+            }
         }
     }
 
--- tests/Php/GenTest.php
+++ tests/Php/GenTest.php
@@ -1,11 +1,10 @@
-<?php
+<?php declare(strict_types=1);
 /**
  * Copyright (C) php-fn. See LICENSE file for license details.
  */
 
 namespace Php;
 
-use Php\test\assert;
 use PHPUnit\Framework\TestCase;
 
 class GenTest extends TestCase
@@ -16,58 +15,75 @@ class GenTest extends TestCase
             '' && yield;
         };
 
-        assert\same([], $this::gen());
-        assert\same([], $this::gen([]));
-        assert\same([], $this::gen($empty));
-        assert\same([], $this::gen($empty, [], []));
-        assert\same(['a' => 'A', 'b' => 'B'], $this::gen(['a' => 'a', 'b' => 'b'], static function ($v) {
+        $this::assertSame([], $this::gen());
+        $this::assertSame([], $this::gen([]));
+        $this::assertSame([], $this::gen($empty));
+        $this::assertSame([], $this::gen($empty, [], []));
+        $this::assertSame(['a' => 'A', 'b' => 'B'], $this::gen(['a' => 'a', 'b' => 'b'], static function ($v) {
             yield strtoupper($v);
         }));
-        assert\same(['A', 'B'], $this::gen(['a' => 'a', 'b' => 'b'], static function ($v) {
+        $this::assertSame(['A', 'B'], $this::gen(['a' => 'a', 'b' => 'b'], static function ($v) {
             yield null => strtoupper($v);
         }));
-        assert\same(array_merge(['a'], ['b']), $this::gen(['a'], ['b']));
-        assert\same(array_merge(['a'], [1 => 'b', 'c' => 'c']), $this::gen(['a'], [1 => 'b', 'c' => 'c']));
-        assert\same(
+        $this::assertSame(array_merge(['a'], ['b']), $this::gen(['a'], ['b']));
+        $this::assertSame(array_merge(['a'], [1 => 'b', 'c' => 'c']), $this::gen(['a'], [1 => 'b', 'c' => 'c']));
+        $this::assertSame(
             array_merge(['a' => 'a'], ['b' => 'b', 'c' => 'c']),
             $this::gen($empty, ['a' => 'a'], ['b' => 'b', 'c' => 'c'])
         );
-        assert\same(
+        $this::assertSame(
             array_merge(['a' => 'a'], ['b' => 'b', 'c' => 'c']),
             $this::gen($empty, ['a' => 'a'], ['b' => 'b', 'c' => 'c'], static function ($value) {
                 yield $value;
             })
         );
-        assert\same(
+        $this::assertSame(
             ['a' => 'a', 'b', 'c' => 'c'],
             $this::gen(['a' => 'a', 'b' => 'b', 'c' => 'c'], static function ($value) {
                 $value === 'b' ? yield null => $value : yield $value;
             })
         );
-        assert\same(
+        $this::assertSame(
             ['A' => 'aa', 'B' => 'bb', 'C' => 'cc'],
             $this::gen(['a', 'b', 'c'], static function ($value) {
                 yield [strtoupper($value)] => $value . $value;
             })
         );
-        assert\same(
+        $this::assertSame(
             ['a' => 'a', 'A' => 'a', 'b' => 'b', 'B' => 'b', 'c' => 'c', 'C' => 'c'],
             $this::gen(['a', 'b', 'c'], static function ($value) {
                 yield [$value] => $value;
                 yield [strtoupper($value)] => $value;
             })
         );
-        assert\same(
+        $this::assertSame(
             ['foo' => ['a' => 'A', 'b' => 'B'], 'bar' => ['c' => 'C']],
             $this::gen(['a', 'b', 'c'], static function ($value) {
                 yield [$value === 'c' ? 'bar' : 'foo', $value] => strtoupper($value);
+            })
+        );
+
+        $this::assertSame(
+            ['a' => 'A', 'b' => 'B'],
+            $this::gen(static function () {
+                yield ['a'] => 'A';
+                yield ['b'] => 'B';
+            })
+        );
+
+        $this::assertSame(
+            ['a' => ['a' => 'aa', 'b' => 'ab'], 'b' => ['a' => 'ba']],
+            $this::gen(static function () {
+                yield ['a', 'a'] => 'aa';
+                yield ['a', 'b'] => 'ab';
+                yield ['b', 'a'] => 'ba';
             })
         );
     }
 
     private static function gen(...$args): array
     {
-        assert\same(iterator_to_array(new Gen(...$args)), $arr = Php::arr(...$args));
+        self::assertSame(iterator_to_array(new Gen(...$args)), $arr = Php::arr(...$args));
         return $arr;
     }
 }
