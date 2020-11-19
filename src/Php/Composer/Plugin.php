35dc78fffb0d6f94e59cc794177772b58e5921ba
Demjan Kaluzki <kaluzkidemjan@gmail.com>
2020-11-19 07:09:00 +0100

    add support for composer 2.0

--- src/Php/Composer/Plugin.php
+++ src/Php/Composer/Plugin.php
@@ -83,4 +83,12 @@ $classes
 EOF
         );
     }
+
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
+    }
+
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+    }
 }
